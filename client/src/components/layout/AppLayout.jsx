aad4788f038be44effc5f635889e624d138a2dda
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-08-27 22:03:29 +0600

    header design add

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -2,6 +2,7 @@
 
 import Title from "../shared/Title";
 import Header from "./Header";
+import { Grid } from "@mui/material"
 
 const AppLayout = () => (WrappedComponent) => {
     return (props) => {
@@ -9,7 +10,35 @@ const AppLayout = () => (WrappedComponent) => {
             <>
                 <Title />
                 <Header />
-                <WrappedComponent {...props} />
+                <Grid container height={"calc(100vh - 4rem)"}>
+                    <Grid
+                        item
+                        sm={4}
+                        md={3}
+                        sx={{
+                            display: { xs: "none", sm: "block" },
+                        }}
+                        height={"100%"}
+                    >
+                        First
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+                        <WrappedComponent {...props} />
+                    </Grid>
+                    <Grid
+                        item
+                        md={4}
+                        lg={3}
+                        height={"100%"}
+                        sx={{
+                            display: { xs: "none", md: "block" },
+                            padding: "2rem",
+                            bgcolor: "rgba(0,0,0,0.85)",
+                        }}
+                    >
+                        First
+                    </Grid>
+                </Grid>
                 <div>Footer</div>
             </>
         )
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,6 +1,118 @@
+import { AppBar, Badge, Box, IconButton, Toolbar, Tooltip, Typography } from "@mui/material";
+import { orange } from "../constants/color";
+import {
+  Add as AddIcon,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Notifications as NotificationsIcon,
+} from "@mui/icons-material";
 
 const Header = () => {
-  return <div>Header</div>;
+  const handleMobile = () => {
+    console.log("mobile");
+  }
+  const openSearch =() => {
+    console.log("openSearch");
+  }
+  const openNewGroup = () => {
+    console.log("openNewGroup");
+  }
+  const navigateToGroup = () => {
+    console.log("navigateToGroup");
+  }
+  const openNotification = () => {
+    console.log("openNotification");
+  }
+  const logoutHandler = () => {
+    console.log("logoutHandler");
+  }
+  return (
+    <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+      <AppBar
+        position="static"
+        sx={{
+          bgcolor: orange,
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            sx={{
+              display: { xs: "none", sm: "block" },
+            }}
+          >
+            Chat App
+          </Typography>
+          <Box
+            sx={{
+              display: { xs: "block", sm: "none" },
+            }}
+          >
+            <IconButton color="inherit" onClick={handleMobile}>
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+            }}
+          />
+          <Box>
+            <IconBtn
+              title={"Search"}
+              icon={<SearchIcon />}
+              onClick={openSearch}
+            />
+            <IconBtn
+              title={"New Group"}
+              icon={<AddIcon />}
+              onClick={openNewGroup}
+            />
+
+            <IconBtn
+              title={"Manage Groups"}
+              icon={<GroupIcon />}
+              onClick={navigateToGroup}
+            />
+
+            <IconBtn
+              title={"Notifications"}
+              icon={<NotificationsIcon />}
+              onClick={openNotification}
+              // value={notificationCount}
+            />
+
+            <IconBtn
+              title={"Logout"}
+              icon={<LogoutIcon />}
+              onClick={logoutHandler}
+            />
+          </Box>
+        </Toolbar>
+
+      </AppBar>
+    </Box>
+
+  
+  );
+};
+
+const IconBtn = ({ title, icon, onClick, value }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onClick}>
+        {value ? (
+          <Badge badgeContent={value} color="error">
+            {icon}
+          </Badge>
+        ) : (
+          icon
+        )}
+      </IconButton>
+    </Tooltip>
+  );
 };
 
 export default Header;
