eff540b8fbbd21612758df6c6d43bcc829a6b241
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-09-01 01:12:16 +0600

    admin deshboard work done

--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,9 +1,69 @@
+import { Avatar } from "@mui/material";
 import AdminLayout from "../../components/dialogs/AdminLayout";
+import Table from "../../components/shared/Table";
+import { useEffect, useState } from "react";
+import { dashboardData } from "../../components/constants/sampleData";
+import { transformImage } from "../../lib/features";
+
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar} />
+    ),
+  },
+
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "friends",
+    headerName: "Friends",
+    headerClassName: "table-header",
+    width: 150,
+  },
+  {
+    field: "groups",
+    headerName: "Groups",
+    headerClassName: "table-header",
+    width: 200,
+  },
+];
 
 const UserManagement = () => {
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    setRows(
+      dashboardData.users.map((i) => ({
+        ...i,
+        id: i._id,
+        avatar: transformImage(i.avatar, 50),
+      }))
+    );
+  },[])
   return( 
   <AdminLayout>
-      <div>UserManagement</div>
+      <Table heading={"All Users"} columns={columns} rows={rows} />
   </AdminLayout>
 );
 };
