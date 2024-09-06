49cea7f2ea474b8f3d6907fd67cad64f4cad1b7d
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-09-06 21:33:25 +0600

    group add member remove member and leave membewr

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newGroupChat } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember } from "../controllers/chat.js";
+import { addMemberValidator, chatIdValidator, removeMemberValidator, validateHandler } from "../lib/validators.js";
 
 
 const app = express.Router();
@@ -15,6 +16,25 @@ app.post("/new",
     // validateHandler, 
     newGroupChat
 );
+
+app.get("/my", getMyChats);
+
+app.get("/my/groups", getMyGroups);
+
+app.put("/addmembers", 
+    addMemberValidator(), 
+    validateHandler, 
+    addMembers
+);
+
+app.put(
+    "/removemember",
+    removeMemberValidator(),
+    validateHandler,
+    removeMember
+);
+
+app.delete("/leave/:id", chatIdValidator(), validateHandler, leaveGroup);
 
 
 export default app;
