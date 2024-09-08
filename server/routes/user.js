aae3ec63f17eb00e51d1395de3434b1276420fd5
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-09-08 23:18:56 +0600

    validator add

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,14 +2,15 @@ import express from "express";
 import { getMyProfile, login, logout, newUser, searchUser } from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { registerValidator, validateHandler } from "../lib/validators.js";
 
 
 const app = express.Router();
 
 app.post("/new",
     singleAvatar,
-    //  registerValidator(), 
-    //  validateHandler, 
+     registerValidator(), 
+     validateHandler, 
     newUser
 );
 
