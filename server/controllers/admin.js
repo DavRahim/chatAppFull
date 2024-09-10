2d54aff19200216999b714c754674ea08d63873e
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-09-10 22:24:43 +0600

    admin controllers add

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -3,6 +3,7 @@ import { TryCatch } from "../middlewares/error.js";
 import { ErrorHandler } from "../utils/utility.js";
 import { User } from "../models/user.js";
 import { Chat } from "../models/chat.js";
+import { Message } from "../models/message.js";
 
 const adminLogin = TryCatch(async (req, res, next) => {
     const { secretKey } = req.body;
@@ -53,9 +54,71 @@ const allUsers = TryCatch(async (req, res) => {
     });
 });
 
+const allChats = TryCatch(async (req, res) => {
+    const chats = await Chat.find({})
+        .populate("members", "name avatar")
+        .populate("creator", "name avatar");
 
+    const transformedChats = await Promise.all(
+        chats.map(async ({ members, _id, groupChat, name, creator }) => {
+            const totalMessages = await Message.countDocuments({ chat: _id });
+
+            return {
+                _id,
+                groupChat,
+                name,
+                avatar: members.slice(0, 3).map((member) => member.avatar.url),
+                members: members.map(({ _id, name, avatar }) => ({
+                    _id,
+                    name,
+                    avatar: avatar.url,
+                })),
+                creator: {
+                    name: creator?.name || "None",
+                    avatar: creator?.avatar.url || "",
+                },
+                totalMembers: members.length,
+                totalMessages,
+            };
+        })
+    );
+
+    return res.status(200).json({
+        status: "success",
+        chats: transformedChats,
+    });
+});
+
+const allMessages = TryCatch(async (req, res) => {
+    const messages = await Message.find({})
+        .populate("sender", "name avatar")
+        .populate("chat", "groupChat");
+
+    const transformedMessages = messages.map(
+        ({ content, attachments, _id, sender, createdAt, chat }) => ({
+            _id,
+            attachments,
+            content,
+            createdAt,
+            chat: chat._id,
+            groupChat: chat.groupChat,
+            sender: {
+                _id: sender._id,
+                name: sender.name,
+                avatar: sender.avatar.url,
+            },
+        })
+    );
+
+    return res.status(200).json({
+        success: true,
+        messages: transformedMessages,
+    });
+});
 
 export {
     adminLogin,
-    allUsers
+    allUsers,
+    allChats,
+    allMessages
 };
